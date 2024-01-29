99dec203b109fdf05084325bf146d10d4465173b
Domitory123 <ddmitromaas@gmail.com>
2024-01-29 17:07:35 +0200

    fix Route

--- shop/routes/web.php
+++ shop/routes/web.php
@@ -23,39 +23,27 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::prefix('product')->controller(ProductController::class)->middleware(['admin'])->group(function () {
-    Route::get('/create','create')->name('product.create');
-    Route::post('/product','store')->name('product.store'); 
-    Route::get('/showDestroy/{product}','showDestroy')->name('productPage.destroy');
-    Route::get('/destroy/{product}','destroy')->name('product.destroy');
-    Route::get('/edit/{product}','edit')->name('product.edit');
-    Route::post('/update/{product}','update')->name('product.update');
-  });
+Route::prefix('product')->middleware(['admin'])->group(function () {
+  Route::resource('product', ProductController::class)->except('show');
+  Route::get('/showDestroy/{product}', 'ProductController@showDestroy')->name('productPage.destroy');
+});
 
-  Route::prefix('product')->controller(ProductController::class)->group(function () {
-    Route::get('/product', 'index')->name('product.index');
-    
+Route::prefix('product')->controller(ProductController::class)->group(function () {
+    Route::get('/product', 'index')->name('product.index');   
    });
 
+Route::prefix('category')->middleware(['admin'])->group(function () {
+    Route::resource('category', CategoryController::class)->except('show');
+    Route::get('/showDestroy/{category}', 'CategoryController@showDestroy')->name('categoryShow.destroy');
+ });
 
-  Route::prefix('category')->controller(CategoryController::class)->middleware(['admin'])->group(function () {
-    Route::get('/categories', 'index')->name('category.index');
-    Route::get('/create','create')->name('category.create');
-    Route::post('/category','store')->name('category.store'); 
-    Route::get('/showDestroy/{category}','showDestroy')->name('categoryShow.destroy');
-    Route::get('/destroy/{category}','destroy')->name('category.destroy');
-    Route::get('/edit/{category}','edit')->name('category.edit');
-    Route::post('/update/{category}','update')->name('category.update');
-  });
-
-  Route::controller(UserController::class)->group(function () {
+Route::controller(UserController::class)->group(function () {
     Route::get('registration', 'getSigUp')->name('registration.getSigUp');
     Route::post('registration', 'postSigUp')->name('registration.postSigUp');
     Route::get('login', 'getSigin')->name('getSigin');
     Route::post('login', 'postSigin')->name('postSigin');
     Route::get('logout', 'logout')->name('logout');
 });
-
 
 Route::controller(AdminController::class)->middleware(['admin'])->group(function () {
   Route::get('admin', 'admin')->name('admin');
